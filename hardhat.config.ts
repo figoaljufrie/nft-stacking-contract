cdc2e7a215f19782996a24898bd6ecff84fd7ce8
Figo Aljufrie <figoaldjoeffri@gmail.com>
2025-10-24 01:47:12 +0700

    Feat/hardhat metamask (#2)
    * feat: nft-stake contract & sol / typescript test (not tested)
    
    * bugfix: ethers / hardhat new versions conflicts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,9 @@
 import type { HardhatUserConfig } from "hardhat/config";
-
-import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import "@nomicfoundation/hardhat-ignition-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
 import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxMochaEthersPlugin],
   solidity: {
     profiles: {
       default: {
